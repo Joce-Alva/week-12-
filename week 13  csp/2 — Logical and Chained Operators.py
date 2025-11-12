0e75f619ee68a994e436ccbf1cf3b16bfa8c33f1
Joce-Alva <jocealvarez8002@gmail.com>
2025-11-12 20:25:36 +0000

    Chained operators

--- week 13  csp/2 — Logical and Chained Operators.py
+++ week 13  csp/2 — Logical and Chained Operators.py
@@ -18,7 +18,23 @@ print(x < 5 or x == 10)   # True
 print(not(x == 10))       # False
 print(1 < x < 20)         # True
 
-
+#score calculator 
+score= int(input("Enter your score (0-100)"))
+# An a is 90 to 100
+if 90 <= score <=100:
+    print("Grade: A")
+# A B is a 80 to 89 
+elif 80 <= score < 90: 
+    print("Grade: B")
+# A C is a 70 to 79
+elif 70 <= score < 80:
+    print("Grade: C")
+# A d is a 60 to 69
+elif 60 <= score < 69:
+    print("Grade: D")
+# Anything below is a F 
+else:
+    print("Grade: F")
 # Practice Problems:
 
 # Write an expression that checks if a number is between 50 and 100 (inclusive).
